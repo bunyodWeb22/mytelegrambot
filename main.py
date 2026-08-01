1948ffb664d927aef799f60d45ca602bddcc5105
bunyodWeb22 <taylerderen@gmail.com>
2026-08-01 13:00:26 +0500

    Tog'irlandi

--- main.py
+++ main.py
@@ -531,7 +531,7 @@ async def check_answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
     return await ask_question(update, context)
 
 def main():
-    TOKEN = "8949503703:AAEcwoItzeMarypb8pTztJox2dDhtu6hi1c"  # Bot tokeningizni yozing
+    TOKEN = "8949503703:AAGK1yYeiOxRiuxikNaHFTd4KVSux8lExEc"  # Bot tokeningizni yozing
 
     app = ApplicationBuilder().token(TOKEN).post_init(setup_bot_commands).build()
 
